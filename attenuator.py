23c17fd04dac084d226b3c6b6b7e5480f0cdadc9
xn3tx <165705340+xn3tx@users.noreply.github.com>
2025-06-06 18:51:23 +0200

    Input output

--- attenuator.py
+++ attenuator.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from drawscene import Drawing
 
 class Attenuator:
@@ -8,15 +7,4 @@ class Attenuator:
     def draw_attenuator(self, attach_x, wheel_y, desk_y):
         spring_top = desk_y + 20
         attenuator_y = (spring_top + wheel_y) // 2
-=======
-from drawscene import Drawing
-
-class Attenuator:
-    def __init__(self):
-        self.b = 20.0
-
-    def draw_attenuator(self, attach_x, wheel_y, desk_y):
-        spring_top = desk_y + 20
-        attenuator_y = (spring_top + wheel_y) // 2
->>>>>>> 92664a21a74fbb884fe937d3ed18307e76553ee4
         Drawing.draw_rect(attach_x - 10, attenuator_y - 20, 20, 40, (255, 165, 0))
--- wheel.py
+++ wheel.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from drawscene import Drawing
 
 class Wheel:
@@ -14,21 +13,4 @@ class Wheel:
 
     def draw_platform(self, center_x, platform_y, ground_y):
         Drawing.draw_line(center_x, platform_y + 20, center_x, ground_y, (180, 180, 180), 12)
-=======
-from drawscene import Drawing
-
-class Wheel:
-    def __init__(self, width=60, height=100):
-        self.width = width
-        self.height = height
-
-    def draw_wheel(self, center_x, bottom_y):
-        Drawing.draw_rect(center_x - self.width // 2, bottom_y - self.height, self.width, self.height, (40, 40, 40))
-        # Bieżnik opony - poziome linie
-        for y in range(bottom_y - self.height + 5, bottom_y, 10):
-            Drawing.draw_line(center_x - self.width // 2, y, center_x + self.width // 2, y, (100, 100, 100), 2)
-
-    def draw_platform(self, center_x, platform_y, ground_y):
-        Drawing.draw_line(center_x, platform_y + 20, center_x, ground_y, (180, 180, 180), 12)
->>>>>>> 92664a21a74fbb884fe937d3ed18307e76553ee4
         Drawing.draw_rect(center_x - 150, platform_y, 300, 20, (100, 100, 255))
